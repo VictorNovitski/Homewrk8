35e9f50ae1cbfacdae237bc716f0b6f64e9002d0
Victor N <vik214@yandex.ru>
2023-02-13 00:06:12 +0300

    решение задачи 58

---
+++ Task58/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Задайте две матрицы. Напишите программу, которая будет
+находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+int[,] matrixA = new int[3, 3];
+int[,] matrixB = new int[3, 3];
+Generate2DArray(matrixA);
+PrintArray(matrixA);
+Console.WriteLine();
+Generate2DArray(matrixB);
+PrintArray(matrixB);
+Console.WriteLine();
+PrintArray(GetResultMatrix(matrixA,matrixB));
+
+void Generate2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i, j] = new Random().Next(1, 10);
+        }   
+    }
+    return;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] GetResultMatrix(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+    return arrayC;
+}
